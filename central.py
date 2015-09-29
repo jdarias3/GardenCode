9b92934c26b43f416ae93eb0e6ff29e32a387a6a
Juan Arias <jdarias3@gmail.com>
2015-09-29 13:37:56 -0500

    Mysql Connector Works! Add information to the database Works! Serial connector with windows works!

--- central.py
+++ central.py
@@ -1,18 +1,15 @@
 #!/usr/bin/env python
 
-import sqlite3
+import mysql.connector
+from mysql.connector import Error
 from cgi import escape, parse_qs
 import serial
 import time
 
-def application(environ, start_response):
 
-     connection = None
-     my_responsev = ""
-     params = parse_qs(environ['QUERY_STRING'])
-     humedad = escape(params.get('humedad',[''])[0])
 
-ser = serial.Serial('/dev/ttyACM0', 9600,timeout=1.0) #Initializing the Serial$
+#ser = serial.Serial('/dev/ttyACM0', 9600,timeout=1.0) #Initializing the Serial$ Linux
+ser = serial.Serial('COM3', 9600,timeout=1.0)
 ackArduino = '0'
 
 
@@ -28,22 +25,23 @@ while True:
         msg = ser.readline()
      #print "MSg from Arduino"
         print (msg)
-        my_query = 'INSERT INTO humedad (id,humedad,datetime) VALUES(NULL,%s,CURRENT_TIMESTAMP);' %(msg,)
+        
 
-        try:
-                connection = sqlite3.connect('/var/www/database/humedad.db' ,i$
-                cursor = connection.cursor()
-                print("TEST1")
-                cursor.execute(my_query)
-                print("TEST2")
-                query_results = cursor.fetchone()
-                my_response = 'Inserted %s at datetime' %(msg)
-        except sqlite3.Error, e:
-                my_response = "There is an error %s:" % (e)
-        finally:
-                connection.close()
-
-        #status = '200 OK'
-        #response_headers = [('Content-Type', 'text/plain'), 'Content-Length',$
-        #start_response(status, response_headers)
-        #return [my_response]
+	 
+		# Connect to MySQL database """
+	try:
+		conn = mysql.connector.connect(user='admin', password='root', host='192.168.0.11', database='Garden')
+		if conn.is_connected():
+			print('Connected to MySQL database')
+			cursor = conn.cursor()
+			cursor.execute("""INSERT INTO GardenMonitor(planta, humedad, temperatura,bomba) VALUES(%s,%s,%s,%s) """,["stevia",msg,38,1])
+			conn.commit()
+			
+ 
+	except Error as e:
+		print(e)
+ 
+	finally:
+		cursor.close()
+		conn.close()
+	 
